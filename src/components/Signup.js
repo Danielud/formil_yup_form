cb5392f3877ed6592bab468239fbeac672aafe1a
danielud <danield@Daniels-MacBook-Pro.local>
2021-11-04 18:06:12 +0200

    intial complete

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { Textfield } from "./Textfield";
 import { Formik, Form } from "formik";
+import * as Yup from "yup";
 
 export const Signup = () => {
+  const validate = Yup.object({
+    firstName: Yup.string()
+      .max(15, "Must be 15 chars or less")
+      .required("Required"),
+    lastName: Yup.string()
+      .max(20, "Must be 18 chars or less")
+      .required("Required"),
+    email: Yup.string().email("Email is invalid").required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password must be min 6 chars")
+      .required("Password is required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Password must match")
+      .required("Must confirm password"),
+  });
   return (
     <Formik
       initialValues={{
@@ -12,11 +28,14 @@ export const Signup = () => {
         password: "",
         confirmPassword: "",
       }}
+      validationSchema={validate}
+      onSubmit={(values) => {
+        console.log(values);
+      }}
     >
       {(formik) => (
         <div>
           <h1>Sign Up</h1>
-          {console.log(formik.values)}
           <Form>
             <Textfield label="First Name" name="firstName" type="text" />
             <Textfield label="Last Name" name="lastName" type="text" />
--- src/components/Textfield.js
+++ src/components/Textfield.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useField } from "formik";
+import { ErrorMessage, useField } from "formik";
 
 export const Textfield = ({ label, ...props }) => {
   const [field, meta] = useField(props);
@@ -7,11 +7,14 @@ export const Textfield = ({ label, ...props }) => {
     <div className="mb-2">
       <label htmlFor={field.name}>{label}</label>
       <input
-        className="form-control shadow-none"
+        className={`form-control shadow-none ${
+          meta.touched && meta.error && "is-invalid"
+        }`}
         {...field}
         {...props}
         autoComplete="off"
       />
+      <ErrorMessage component="div" name={field.name} className="error" />
     </div>
   );
 };
